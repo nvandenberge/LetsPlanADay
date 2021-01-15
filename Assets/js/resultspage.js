501d6da1a4ea6d9638eb869e5a4880918e2625e7
Nick Vandenberge <nvandenberge14@gmail.com>
2021-01-15 16:37:51 -0500

    added ticketmaster api request and div elements to display event data

--- Assets/js/resultspage.js
+++ Assets/js/resultspage.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
       },
     });
 
-    // Function that builds url for API request
+    // Function that builds url for Yelp API request & UI content
     yelpREST("/businesses/search", { params: { location: zipCode } }).then(
       ({ data }) => {
         // Get a random number between 0 and legnth of array
@@ -22,32 +22,106 @@ $(document).ready(function () {
         let restData = data.businesses[randomRestIndex];
 
         // Creating <img> element and adding image url to src attr.
-        let image = $("<img>").attr({
-          src: restData.image_url,
+        let foodImage = $("<img>").attr({
+          src: restData?.image_url,
           height: "auto",
           width: "100%",
           fit: "scale",
         });
-        image.empty();
+        foodImage.empty();
 
         // Puts full address together
         let fullRestAddress = restData.location.display_address.join(" | ");
 
         // Using IF statements to ensure data is available, if not, dont display
+        // Restaurant image
         if (restData.image_url !== undefined || "") {
-          $("#restImage").append(image);
+          $("#restImage").append(foodImage);
         }
+        // Restaurant name
         $("#restName").text(`Name: ${restData.name}`);
+        // Restaurant type
         if (restData.categories[0].title !== undefined || "") {
           $("#restType").text(`Type: ${restData.categories[0]?.title}`);
         }
+        // Restaurant phone number
         if (restData.phone !== undefined || "") {
           $("#restPhone").text(`Phone: ${restData?.phone}`);
         }
-        if (restData.location.display_address !== undefined || "")
+        // Restaurant address
+        if (restData.location.display_address !== undefined || "") {
           $("#restAddress").text(`Address: ${fullRestAddress}`);
+        }
+        eventRequest(zipCode);
       }
     );
+  }
+
+  // Function that gets TicketMaster API request from  & UI content
+  function eventRequest(zipCode) {
+    let eventAPIKey = "Fm5l5yKh5aBTmisz5MDUhqCSxcPtyTWL";
+
+    $.ajax({
+      type: "GET",
+      url: `https://cors-anywhere.herokuapp.com/https://app.ticketmaster.com/discovery/v2/events.json?apikey=${eventAPIKey}&postalCode=${zipCode}`,
+      async: true,
+      dataType: "json",
+      success: function (response) {
+        // Error handling if no events are returned.
+        if (response?._embedded?.events?.length === undefined) {
+          $("#eventError").text(
+            "Sorry, there are no upcoming events in this area."
+          );
+          $("#eventErrorSig").text("Love, COVID-19");
+        }
+        // If events are found, run the following
+        else {
+          // Get a random number between 0 and legnth of array
+          let randomEventIndex = Math.floor(
+            Math.random() * response._embedded.events.length
+          );
+          let eventData = response._embedded.events[randomEventIndex];
+
+          // Creating <img> element and adding image url to src attr.
+          let eventImage = $("<img>").attr({
+            src: eventData?.images[0].url,
+            height: "auto",
+            width: "100%",
+            fit: "scale",
+          });
+          eventImage.empty();
+
+          // Event image
+          if (eventData?.images[0].url !== undefined || "") {
+            $("#eventImage").append(eventImage);
+          }
+          // Event name
+          $("#eventName").text(`Name: ${eventData.name}`);
+          // Convert date format to MM-DD-YYYY using moment.js
+          if (eventData.dates.start.localDate !== undefined || "") {
+            $("#eventDate").text(
+              `Date: ${moment(eventData?.dates?.start?.localDate).format("L")}`
+            );
+          }
+          // Event description
+          if (eventData.info !== undefined || "") {
+            $("#eventInfo").text(`Description: ${eventData?.info}`);
+          }
+          // Link to event
+          if (eventData.url !== undefined || "") {
+            // Creating anchor element to hold ticket link
+            let ticketLink = $("<a>")
+              .text("Get Tickets")
+              .attr("href", eventData.url);
+            $("#eventTickets").text(`Tickets: `);
+            $("#eventTickets").append(ticketLink);
+          }
+        }
+      },
+      error: function (xhr, status, err) {
+        console.log("ERROR == ", xhr.status);
+      },
+    });
   }
   // Gets the zip code from the URL
   const urlParams = new URLSearchParams(window.location.search);
